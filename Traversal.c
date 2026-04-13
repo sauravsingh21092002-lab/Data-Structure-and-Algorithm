8c8b05aa052bfb91efc968569e2ca0aa99262812
Saurav Kumar <sauravsingh21092002@gmail.com>
2026-04-13 07:47:31 +0530

    Add files via upload

---
+++ Traversal.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int Arr[5] = {5,10,15,20,25};
+    int i;
+    for(i = 0; i < 5; i++){
+        printf("Traverse to:");
+        printf("%d\n", Arr[i]);
+
+    }
+    return 0;
+}
